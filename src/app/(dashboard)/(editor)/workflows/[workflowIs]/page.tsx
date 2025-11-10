9ed4153d94323ed04c4b66bfea6d64b7892c45c1
An <nguyenlebinhank63@gmail.com>
2025-11-10 22:12:51 +0700

    Add editorName component,hooks

--- src/app/(dashboard)/(editor)/workflows/[workflowIs]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowIs]/page.tsx
@@ -1,4 +1,10 @@
+import { Editor, EditorError, EditorLoading } from "@/features/editor/components/editor";
+import { EditorHeader } from "@/features/editor/components/editor-header";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PageProps {
     params: Promise<{
@@ -9,8 +15,18 @@ interface PageProps {
 const Page= async({params} : PageProps) =>{
     await requireAuth();
     const {workflowIs} = await params;
+    prefetchWorkflow(workflowIs);
     return (
-        <div>Credential id :{workflowIs}</div>
-    )
-}
+        <HydrateClient>
+        <ErrorBoundary fallback={<EditorError/>}>
+            <Suspense fallback={<EditorLoading/>}>
+                <EditorHeader workflowId= {workflowIs}/>
+                <main className="flex-1 ">
+                    <Editor workflowId={workflowIs}/>                
+                </main>
+            </Suspense>
+        </ErrorBoundary>
+        </HydrateClient>
+    );
+};
 export default Page;
--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -48,3 +48,33 @@ export const useRemoveWorkflow = () =>{
         })
     )
 }
+
+
+export const useSuspenseWorkflow =(id:string) =>{
+    const trpc = useTRPC();
+    return useSuspenseQuery(trpc.workflows.getOne.queryOptions({id}));
+}
+
+
+export const useUpdateWorkflowName =() =>{
+
+    const queryClient =useQueryClient();
+    const trpc = useTRPC();
+
+    return useMutation(
+        trpc.workflows.updateName.mutationOptions({
+            onSuccess:(data) =>{
+                toast.success(`workflow "${data.name}" updated`);
+                queryClient.invalidateQueries(
+                    trpc.workflows.getMany.queryOptions({}),
+                );
+                queryClient.invalidateQueries(
+                    trpc.workflows.getOne.queryOptions({id:data.id})
+                )
+            },
+            onError: (error) =>{
+                toast.error (`Failed to create workflow: ${error.message}`);
+            }
+        })
+    )
+}

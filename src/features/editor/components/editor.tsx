e3b8d52e46a401548a5542d03dd8af0d4fe0d20c
An <nguyenlebinhank63@gmail.com>
2025-11-12 21:32:54 +0700

    Add node-components

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,6 +2,10 @@
 
 import { ErrorView, LoadingView } from "@/components/entity-components"
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows"
+import { useState, useCallback } from 'react';
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge,type Node, type Edge,type NodeChange,type EdgeChange,type Connection, Background, Controls, MiniMap } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import { nodeComponents } from "@/config/node-components";
 
 export const EditorLoading = () =>{
     return <LoadingView message="Loading editor ..."/>
@@ -11,12 +15,42 @@ export const EditorError = () =>{
     return <ErrorView message="Error loading editor"/>
 }
 
+
 export const Editor = ({workflowId}:{workflowId:string}) =>{
     const {data:workflow} = useSuspenseWorkflow(workflowId);
-
+    const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+    const [edges, setEdges] = useState<Edge[]>(workflow.edges);  
+    
+    const onNodesChange = useCallback(
+        (changes:NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        [],
+    );
+    const onEdgesChange = useCallback(
+        (changes:EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        [],
+    );
+    const onConnect = useCallback(
+        (params:Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+        [],
+    );    
     return (
-        <p>
-            {JSON.stringify(workflow,null,2)};
-        </p>
+        <div className="size-full">
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                nodeTypes={nodeComponents}
+                fitView      
+                proOptions={{
+                    hideAttribution:true,
+                }}          
+            >
+                <Background/>
+                <Controls/>
+                <MiniMap/>
+            </ReactFlow>
+        </div>
     )
 }
--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -3,15 +3,24 @@ import {generateSlug} from "random-word-slugs"
 import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/init";
 import z from "zod";
 import { PAGINATION } from "@/config/constants";
+import { NodeType } from "@/generated/prisma";
+import { Edge,Node } from "@xyflow/react";
 
 export const workflowsRouter = createTRPCRouter({
     create:premiumProcedure.mutation(({ctx})=>{
         return prisma.workflow.create({
             data:{
                 name:generateSlug(3),
-                userId:ctx.auth.user.id
-            }
-        })
+                userId:ctx.auth.user.id,
+                nodes:{
+                    create:{
+                        type:NodeType.INITIAL,
+                        position:{x:0,y:0},
+                        name:NodeType.INITIAL
+                    },
+                },
+            },
+        });
     }),
     remove:protectedProcedure
         .input(z.object({id:z.string()}))
@@ -36,14 +45,36 @@ export const workflowsRouter = createTRPCRouter({
         }),
     getOne:protectedProcedure
         .input(z.object({id:z.string()}))
-        .query(({ctx,input})=>{
-            return prisma.workflow.findFirstOrThrow({
+        .query(async({ctx,input})=>{
+            const workflow = await prisma.workflow.findFirstOrThrow({
                 where:{
                     id: input.id,
                     userId:ctx.auth.user.id,
-                } 
-            })
+                },
+                include:{nodes:true,connections:true},
 
+            })
+        const nodes : Node[] = workflow.nodes.map((node) =>({
+            id:node.id,
+            type:node.type,
+            position:node.position as {x:number, y:number},
+            data: (node.data as Record<string,unknown>) || {},
+        }))
+
+        const edges : Edge[] = workflow.connections.map((connection) => ({
+            id:connection.id,
+            source:connection.fromNodeId,
+            target:connection.toNodeId,
+            sourceHandle:connection.fromOutput,
+            targetHandle:connection.toInput,
+        }))
+
+        return {
+            id:workflow.id,
+            name:workflow.name,
+            nodes,
+            edges,
+        }
     }),
     getMany:protectedProcedure
         .input(
